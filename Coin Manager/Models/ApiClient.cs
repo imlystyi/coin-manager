4add4e349d7ceee36130395ca0d9fb122d03f7e7
Vladyslav Yakubovskyi <vladte3c@gmail.com>
2023-07-20 21:53:19 +0300

    Add project files.

---
+++ Coin Manager/Models/ApiClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinManager.Models
+{
+    public static class ApiClient
+    {
+        #region Fields
+
+        private static readonly HttpClient _client = new HttpClient();
+
+        private const string URL_BASE = "";
+
+        #endregion
+    }
+}
